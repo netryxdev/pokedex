b82858660824df0e5c23f82c34fdbe26838292a6
Neto <adrianoneto2501@gmail.com>
2022-08-26 03:50:24 -0300

    fix error types in console angular sprites, name, id

--- src/app/pokemon-list/poke-list.component.ts
+++ src/app/pokemon-list/poke-list.component.ts
@@ -21,6 +21,27 @@ export class PokeListComponent implements OnInit {
   public getAllPokemons: any[] = [];
 
   public apiError: boolean = false;
+  
+  typeColors: any = [
+    {normal: '#A8A77A'},
+    {fire: '#EE8130'},
+    {water: '#6390F0'},
+    {electric: '#F7D02C'},
+    {grass: '#7AC74C'},
+    {ice: '#96D9D6'},
+    {fighting: '#C22E28'},
+    {poison: '#A33EA1'},
+    {ground: '#E2BF65'},
+    {flying: '#A98FF3'},
+    {psychic: '#F95587'},
+    {bug: '#A6B91A'},
+    {rock: '#B6A136'},
+    {ghost: '#735797'},
+    {dragon: '#6F35FC'},
+    {dark: '#705746'},
+    {steel: '#B7B7CE'},
+    {fairy: '#D685AD'}
+  ]
 
   constructor(
     private pokeApiService: PokeApiService
@@ -31,9 +52,9 @@ export class PokeListComponent implements OnInit {
       .subscribe((res: any) => {
         this.setAllPokemons = res.results;
         this.getAllPokemons = this.setAllPokemons;
-        console.log(res)
+        // console.log(res)
         // console.log(res.results)
-        console.log(this.getAllPokemons)
+        // console.log(this.getAllPokemons)
       }
     );
   }
@@ -45,5 +66,7 @@ export class PokeListComponent implements OnInit {
 
     this.getAllPokemons = filter;
   }
+
 }
+
 
