d840ae5b7c17dbbc8d4d3cda753d23b944f0fef6
Neto <adrianoneto2501@gmail.com>
2022-08-29 17:13:13 -0300

    deploy configuration

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const path = require('path');
 
 const app = express();   
 
+const PORT = process.env.PORT || 8080;
+
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/pokedex-app'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/pokedex-app/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server started on port' + PORT);
+});
